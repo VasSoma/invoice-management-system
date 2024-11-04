a1fa97a0ca161d497ff10c38403adb5be2777eb5
VasSoma <vasmaticssoma@gmail.com>
2024-11-04 14:56:13 +0100

    Maven and git related default files

---
+++ invoice_management_system_be/src/test/java/hu/novin/probafeladat/invoice_management_system/InvoicingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.novin.probafeladat.invoice_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
