ae6c59337e85ebde9794b8832593458771a4e3de
VasSoma <vasmaticssoma@gmail.com>
2024-11-04 15:05:54 +0100

    Remove unused import statements

--- invoice_management_system_be/src/main/java/hu/novin/probafeladat/invoice_management_system/InvoicingSystemApplication.java
+++ invoice_management_system_be/src/main/java/hu/novin/probafeladat/invoice_management_system/InvoicingSystemApplication.java
@@ -1,10 +1,7 @@
 package hu.novin.probafeladat.invoice_management_system;
 
-import hu.novin.probafeladat.invoice_management_system.user.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InvoicingSystemApplication {
